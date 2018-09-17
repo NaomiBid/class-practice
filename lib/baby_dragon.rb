e3023ad8b58c840dfc171bd97c768bd6693b0383
NaomiBid <naomi0bid@gmail.com>
2018-09-17 23:12:25 +0100

    updated baby dragon

--- lib/baby_dragon.rb
+++ lib/baby_dragon.rb
@@ -2,6 +2,21 @@ class BabyDragon
 
   def initialize(name)
     @name = name
+    @stomach = "empty"
   end
-  
+
+  def feed
+    @stomach = "full"
+  end
+
 end
+
+def dragon_game
+  puts "Welcome!"
+  puts "Name your dragon."
+  name = gets.chomp
+  BabyDragon.new(name)
+  puts "Awww, #{name} is so cute!"
+end
+
+dragon_game
--- spec/baby_dragon_spec.rb
+++ spec/baby_dragon_spec.rb
@@ -5,3 +5,15 @@ describe "baby_dragon" do
     expect(BabyDragon.new("Fluffy")).to be_instance_of BabyDragon
   end
 end
+
+describe "feed" do
+  it "should return 'full'" do
+    expect(BabyDragon.new("Eddie").feed).to eq("full")
+  end
+end
+
+#describe "dragon_game" do
+#  it "should puts name" do
+
+#  end
+#end
